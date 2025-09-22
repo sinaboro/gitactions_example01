7195db40f6a79c1e67bad8c80b3e8bf80407b350
sinaboro <rlaeocjf@gmail.com>
2025-09-22 12:13:52 +0900

    re2

--- src/main/java/com/example/gitactions/GitActions.java
+++ src/main/java/com/example/gitactions/GitActions.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GitActions {
     @GetMapping("/")
     public String home() {
-       return "Hello, CICD1!!";
+       return "Hello, CICD2!!";
     }
 }
